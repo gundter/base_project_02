9ce7af57ec7a38fa427e9b2e52a6b1640dd77060
Terry Gunderson <tgunderson25@gmail.com>
2015-04-30 14:41:47 -0500

    added location.html, added .get functionality in script.js

--- script.js
+++ script.js
@@ -3,4 +3,16 @@
  */
 $(document).ready(function(){
 
+    $('#get-info-btn').on('click', function(){
+        $.get('location.html', function(data){
+            console.log("first request");
+            $('#more-stuff').append(data);
+        });
+
+        $.get('data.json', function(data){
+            console.log("second request");
+            var firstCity = data.locations[0].location;
+            $('#more-stuff').children().children().append("<p>" + firstCity + "</p>");
+        });
+    });
 });
