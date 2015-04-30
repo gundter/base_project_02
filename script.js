12576907a8bef4294ccac88567ab390987151cc7
Terry Gunderson <tgunderson25@gmail.com>
2015-04-30 15:29:30 -0500

    finished lecture

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 /**
  * Created by Prime on 4/30/15.
  */
+var cityData = null;
+var cityHtml = null;
+
 $(document).ready(function(){
 
     $('#get-info-btn').on('click', function(){
-        $.get('location.html', function(data){
-            console.log("first request");
-            $('#more-stuff').append(data);
-        });
+        if(cityHtml === null){
+            $.get('location.html', function(data){
+                cityHtml = data;
+                console.log("it worked");
+                $('#more-stuff').append(data);
+            });
+        } else{
+            console.log("You already got the HTML");
+        }
 
-        $.get('data.json', function(data){
-            console.log("second request");
-            var firstCity = data.locations[0].location;
-            $('#more-stuff').children().children().append("<p>" + firstCity + "</p>");
-        });
+        if(cityData === null){
+            $.get('data.json', function(data){
+                console.log("second request");
+                cityData = data;
+                var firstCity = data.locations[0].location;
+            });
+        }else {
+            console.log(cityData.locations[0].population);
+            console.log("you already got the city data");
+        }
+    });
+
+    $('#more-stuff').on('click', 'button', function(){
+       $(this).parent().append(cityData.locations[0].population);
     });
 });
